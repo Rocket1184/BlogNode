8bbc227aa80f15f926b9f757ff61208c9fd62d7c
Rocket <Rocket1184@outlook.com>
2016-08-13 22:30:09 +0800

    completed: load article list

--- script/index.js
+++ script/index.js
@@ -2,28 +2,35 @@
 
 window.onload = () => {
     console.log('Welcome to Rocka\'s Node Blog! ');
-    
+
+    var ul = document.getElementsByTagName('ul')[0];
     var bq = document.createElement('blockquote');
     document.body.appendChild(bq);
 
-    function success(text) {
-        var bq = document.getElementById('test-response-text');
-        bq.innerText = text;
+    function success(response) {
+        var resData = JSON.parse(response);
+        resData.forEach((title) => {
+            var newLine = document.createElement('li');
+            newLine.innerHTML = `<a href="javascript:void(0);">${title}</a>`;
+            ul.appendChild(newLine);
+        });
     }
 
     function fail(code) {
-        var bq = document.getElementById('test-response-text');
-        bq.innerText = 'Error code: ' + code;
+        var newLine = document.createElement('li');
+        newLine.innerText = `List Load Faild: Please Refresh Page And Try Again.`;
+        ul.appendChild(newLine);
     }
 
     var request = new XMLHttpRequest(); // New XMLHttpRequest Object
 
-    request.onreadystatechange =  () => { // invoked when readyState changes
+    request.onreadystatechange = () => { // invoked when readyState changes
         if (request.readyState === 4) { // request succeed
             // response result:
             if (request.status === 200) {
                 // succeed: update article
-                return success(request.responseText);
+                console.log(request.response);
+                return success(request.response);
             } else {
                 // failed: show error code
                 return fail(request.status);
